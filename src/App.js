d29a2d3973bbbb313dbd099e81267c153bf5a096
AlexandreRC <74365451+AlexandreRC@users.noreply.github.com>
2021-03-02 21:25:06 -0300

    Finished

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import Button from './components/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Buttons</h3>
+      <div className='wrapper'>
+
+        <div className='container'>
+          <span>{'<'}Button {'/>'}</span>
+          <Button>Default</Button>
+        </div>
+        <div className='container'>
+          <span>{'<'}Button variant="outline" {'/>'}</span>
+          <Button color='red' variant="outline" >Default</Button>
+        </div>
+        <div className='container'>
+          <span>{'<'}Button variant="text" {'/>'}</span>
+          <Button color='darkblue' variant="text">Default</Button>
+        </div>
+        <div className='container'>
+          <span>{'<'}Button disableShadow {'/>'}</span>
+          <Button color='darkblue' disableShadow>Default</Button>
+        </div>
+        <div className='container'>
+          <span>{'<'}Button startIcon='favorite_border' {'/>'}</span>
+          <Button color='red' startIcon='favorite_border' >Default</Button>
+        </div>
+        <div className='container'>
+          <span>{'<'}Button endIcon='add_shopping_cart' {'/>'}</span>
+          <Button color='green' endIcon='add_shopping_cart' >Default</Button>
+        </div>
+        <div className='container'>
+          <span>{'<'}Button size='sm' {'/>'}</span>
+          <Button color='green' size='sm' >Default</Button>
+        </div>
+        <div className='container'>
+          <span>{'<'}Button size='md' {'/>'}</span>
+          <Button color='darkviolet' size='md' >Default</Button>
+        </div>
+        <div className='container'>
+          <span>{'<'}Button size='lg' {'/>'}</span>
+          <Button color='peru' size='lg' >Default</Button>
+        </div>
+        <div className='container'>
+          <span>{'<'}Button color='red' {'/>'}</span>
+          <Button color='red' >Default</Button>
+        </div>
+        <div className='container'>
+          <span>{'<'}Button color='blue' {'/>'}</span>
+          <Button color='blue' >Default</Button>
+        </div>
+        <div className='container'>
+          <span>{'<'}Button color='#9e7a10' {'/>'}</span>
+          <Button color='#9e7a10' >Default</Button>
+        </div>
+      </div>
+    </div>  
+    
+  );
+}
+
+export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+import { lighten, darken } from 'polished'
+
+const handleSize = size => {
+    switch (size) {
+      case "sm":
+        return "6px 12px";
+      case "md":
+        return "8px 16px";
+      case "lg":
+        return "11px 22px";
+      default:
+        return "8px 16px";
+    }
+  };
+
+const StyledButton = styled.button`
+    @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@500&display=swap');
+    background: ${({variant, color}) => variant? 'white' : (color? color : '#E0E0E0')};
+    border: ${({variant, color}) => (variant === 'outline'? `1px solid ${color? color : '#3F3F3F'}` : `none`)};
+    color: ${({variant, color}) => variant? (color? color : '#3F3F3F') : color? 'white' : '#3F3F3F' };
+    display: flex;
+    gap: 8px;
+    align-items: center;
+    font-family: 'Noto Sans JP', sans-serif;
+    font-size: 14px;
+    cursor: pointer;
+    border-radius: 6px;
+    box-shadow: ${({disableShadow}) => disableShadow? 'none' : '0px 2px 3px rgba(51, 51, 51, 0.2)'};
+    padding: ${({size}) => handleSize(size) };
+    outline: none;
+    &:hover , &:focus{
+        background: ${({variant, color}) => (variant === 'text' || variant === 'outline')
+            ? lighten(0.45, color? color : '#757575') 
+            : darken(0.10, color? color : '#E0E0E0')};
+    }
+`
+const StyledIcon = styled.div`
+    font-size: 16px;
+`
+const Button = (props) => {
+    return (
+        <StyledButton  {...props}>
+            {props.startIcon? <StyledIcon className='material-icons'>{props.startIcon}</StyledIcon> : null}
+            {props.children}
+            {props.endIcon? <StyledIcon className='material-icons'>{props.endIcon}</StyledIcon> : null}
+        </StyledButton>
+    )
+}
+
+export default Button;
